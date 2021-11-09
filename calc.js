1eb2a14bd2f4114002ab36800291d8f71533733b
Sam Forbyn <samforbyn@gmail.com>
2021-11-09 14:19:45 -0700

    Initial commit

---
+++ calc.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+	console.log('mathSymbol', mathSymbol)
+	console.log('num1', num1);
+	console.log('num2', num2);
+
+	if (mathSymbol == "-") {
+		let answer = num1 - num2
+		console.log(answer)
+	} else if (mathSymbol == "+"){
+		let answer = num1 + num2
+		console.log(answer)
+	} else if (mathSymbol == "")
+	
+
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
