3d4db73e505da053ff23a9cf877fab8a88d219a7
Sam Forbyn <samforbyn@gmail.com>
2021-11-09 16:43:40 -0700

    finished product :)

--- calc.js
+++ calc.js
@@ -13,7 +13,7 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
-	num3 = Number(tokesn[3]);
+	num3 = Number(tokens[3]);
 
 	console.log('mathSymbol', mathSymbol)
 	console.log('num1', num1);
@@ -38,8 +38,8 @@ reader.question("What would you like to calculate?", function(input){
 		let answer = num1 * num1
 	 	console.log(`${num1} squared is ${answer}`)
 	} else if (mathSymbol == "%"){
-		let answer = num1 % num2
-		console.log(`${num1} divided by ${num2} will have a remainder of ${answer}`)
+		let answer = num1 % num2 %num3
+		console.log(`${num1} divided by ${num2} divided by ${num3} will have a remainder of ${answer}`)
 	} else if (mathSymbol == "cube"){
 		let answer = num1 * 3
 		console.log(`${num1} cubed is ${answer}`)
