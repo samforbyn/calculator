0b61aca8c66ed3f1399e052fd60b8b74db15293d
Sam Forbyn <samforbyn@gmail.com>
2021-11-09 15:31:13 -0700

    num3, cube, raise to the power of, and remainder functions added

--- calc.js
+++ calc.js
@@ -13,22 +13,23 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokesn[3]);
 
 	console.log('mathSymbol', mathSymbol)
 	console.log('num1', num1);
 	console.log('num2', num2);
 
 	if (mathSymbol == "-") {
-		let answer = num1 - num2
+		let answer = num1 - num2 - num3
 		console.log(`The answer is ${answer}`)
 	} else if (mathSymbol == "+"){
-		let answer = num1 + num2
+		let answer = num1 + num2 + num3
 		console.log(`The answer is ${answer}`)
 	} else if (mathSymbol == "/"){
-		let answer = num1 / num2
+		let answer = num1 / num2 /num3
 		console.log(`The answer is ${answer}`)
 	} else if (mathSymbol == "*"){
-		let answer = num1 * num2
+		let answer = num1 * num2 * num3
 		console.log(`The answer is ${answer}`)
 	} else if (mathSymbol == "sqrt"){
 		let answer = Math.sqrt(num1)
@@ -39,6 +40,12 @@ reader.question("What would you like to calculate?", function(input){
 	} else if (mathSymbol == "%"){
 		let answer = num1 % num2
 		console.log(`${num1} divided by ${num2} will have a remainder of ${answer}`)
+	} else if (mathSymbol == "cube"){
+		let answer = num1 * 3
+		console.log(`${num1} cubed is ${answer}`)
+	}else if (mathSymbol == "^"){
+		let answer = Math.pow(num1, num2)
+		console.log(`${num1} to the power of ${num2} would be ${answer}`)
 	}
 		
 		
